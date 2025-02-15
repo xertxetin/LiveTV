71b052a02ceac5941d0eca34b830039e7f4f07f5
Mert Çetin <122017752+xertxetin@users.noreply.github.com>
2025-02-15 18:51:36 +0300

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'tv-yayin-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
 
 // Service Worker yüklenirken önbelleğe alma işlemi
